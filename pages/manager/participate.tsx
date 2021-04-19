95d7f840fae3aff492f706917107a843749eb429
ProSavage <naman0311@gmail.com>
2021-04-18 19:36:05 -0500

    reorganize code

--- pages/manager/participate.tsx
+++ pages/manager/participate.tsx
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import styled from "styled-components";
-import { QuizQuestion } from "../../util/types/Quiz";
-import Quiz from "../../util/types/Quiz";
-import QuestionDisplay from "../../components/manager/QuestionDisplay";
 import { quizTakerQuiz } from "../../atoms/QuizTakerQuiz";
-import getAxios, { buildAxios } from "../../util/AxiosBuilder";
 import { userState } from "../../atoms/user";
-import { debug } from "node:console";
+import QuestionDisplay from "../../components/manager/QuestionDisplay";
+import getAxios, { buildAxios } from "../../util/AxiosBuilder";
+import Quiz, { QuizQuestion } from "../../util/types/Quiz";
 
 export default function Participate() {
-  
-    const [quizID, setQuizID] = useState("");
-    const [searchMessage, setSearchMessage] = useState("");
-    const [quiz, setQuiz] = useRecoilState(quizTakerQuiz);
-    const user = useRecoilValue(userState);
+  const [quizID, setQuizID] = useState("");
+  const [searchMessage, setSearchMessage] = useState("");
+  const [quiz, setQuiz] = useRecoilState(quizTakerQuiz);
+  const user = useRecoilValue(userState);
 
-    const loadQuestions = () => {
-      
-      getAxios()
+  const loadQuestions = () => {
+    getAxios()
       .post("/loader", {
         _id: quizID,
       })
@@ -27,16 +23,15 @@ export default function Participate() {
           buildAxios();
           setQuiz(res.data.quiz);
           setSearchMessage(res.data.message);
-        }
-        else {
+        } else {
           setSearchMessage(res.data.message);
           setQuiz(res.data.quiz);
         }
       });
-    };
+  };
 
-    const submitAnswers = () => {
-      getAxios()
+  const submitAnswers = () => {
+    getAxios()
       .post("/grader", {
         _id: quizID,
         user: user,
@@ -47,128 +42,131 @@ export default function Participate() {
           buildAxios();
           setQuiz(res.data.quiz);
           setSearchMessage(res.data.message);
-        }
-        else {
+        } else {
           setSearchMessage(res.data.message);
           setQuiz(res.data.quiz);
         }
       });
-    }
-  
-    return (
-      <Wrapper>
-        <p>{searchMessage}</p>
-        <FormInput
+  };
+
+  return (
+    <Wrapper>
+      <p>{searchMessage}</p>
+      <FormInput
         placeholder={"QUIZ ID: "}
         type={"_id"}
         value={quizID}
         onChange={(e) => setQuizID(e.target.value)}
-        />
-        <button 
-          onClick={(e) => {
+      />
+      <button
+        onClick={(e) => {
           e.preventDefault();
           loadQuestions();
-          }}
-          type={"submit"}
-        >START</button>
-        <Metadata>
-          <h3>{quiz.name}</h3>
-          <label>{quiz.description}</label>
-        </Metadata>
-        <button
+        }}
+        type={"submit"}
+      >
+        START
+      </button>
+      <Metadata>
+        <h3>{quiz.name}</h3>
+        <label>{quiz.description}</label>
+      </Metadata>
+      <button
         onClick={(e) => {
           e.preventDefault();
           submitAnswers();
-          }}
-          type={"submit"}
-        >SUBMIT QUIZ</button>
-        <QuestionsContainer>
-          <QuestionManagerHeader>
-            <strong>{quiz.questions.length} Questions</strong>
-          </QuestionManagerHeader>
-          {quiz.questions.map((question, key) => (
-            <QuestionDisplay key={key} question={question} id={key} />
-          ))}
-        </QuestionsContainer>
-      </Wrapper>
-    );
-  }
-  
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    margin: 1em 0;
-  `;
-  
-  const Metadata = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 1em 0;
-    max-width: 720px;
-  `;
-  
-  const QuestionsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: 1em 0;
-  `;
-  
-  const QuestionManagerHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-  `;
-  
-  const FormInput = styled.input`
-    margin: 0.5em 0;
-  `;
-  
-  const blankQuestion: QuizQuestion = {
-    question: "What is 3 + 4?",
-    answers: [
-      {
-        answer: "5",
-        correct: false,
-      },
-      {
-        answer: "7",
-        correct: true,
-      },
-      {
-        answer: "8",
-        correct: false,
-      },
-    ],
-    };
+        }}
+        type={"submit"}
+      >
+        SUBMIT QUIZ
+      </button>
+      <QuestionsContainer>
+        <QuestionManagerHeader>
+          <strong>{quiz.questions.length} Questions</strong>
+        </QuestionManagerHeader>
+        {quiz.questions.map((question, key) => (
+          <QuestionDisplay key={key} question={question} id={key} />
+        ))}
+      </QuestionsContainer>
+    </Wrapper>
+  );
+}
 
-    const blankQuestion2: QuizQuestion = {
-      question: "What is 2 + 2?",
-      answers: [
-        {
-          answer: "3",
-          correct: false,
-        },
-        {
-          answer: "4",
-          correct: true,
-        },
-        {
-          answer: "5",
-          correct: false,
-        },
-      ],
-      };
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: 1em 0;
+`;
 
-    const ExampleQuiz: Quiz = {
-        _id: "aaaaaa",
-        name: "Example Quiz",
-        description: "A quiz made for debugging",
-        questions: [blankQuestion, blankQuestion2],
-    };
+const Metadata = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1em 0;
+  max-width: 720px;
+`;
 
-    const EmptyQuiz: Quiz = {
-      _id: "",
-      name: "",
-      description: "",
-      questions: []
-    }
+const QuestionsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1em 0;
+`;
+
+const QuestionManagerHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const FormInput = styled.input`
+  margin: 0.5em 0;
+`;
+
+const blankQuestion: QuizQuestion = {
+  question: "What is 3 + 4?",
+  answers: [
+    {
+      answer: "5",
+      correct: false,
+    },
+    {
+      answer: "7",
+      correct: true,
+    },
+    {
+      answer: "8",
+      correct: false,
+    },
+  ],
+};
+
+const blankQuestion2: QuizQuestion = {
+  question: "What is 2 + 2?",
+  answers: [
+    {
+      answer: "3",
+      correct: false,
+    },
+    {
+      answer: "4",
+      correct: true,
+    },
+    {
+      answer: "5",
+      correct: false,
+    },
+  ],
+};
+
+const ExampleQuiz: Quiz = {
+  _id: "aaaaaa",
+  name: "Example Quiz",
+  description: "A quiz made for debugging",
+  questions: [blankQuestion, blankQuestion2],
+};
+
+const EmptyQuiz: Quiz = {
+  _id: "",
+  name: "",
+  description: "",
+  questions: [],
+};
